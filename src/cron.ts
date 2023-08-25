3ae48d60ed084b93ba4e37a4dd4bbbcaae6f1a02
Adebayo Adeoye <adeoyeadebayo18@gmail.com>
2023-08-25 19:29:21 +0100

    Implemented delete Unsafe files Cron

---
+++ src/cron.ts
@@ -0,0 +1,23 @@
+import cron from "node-cron";
+import { PrismaClient } from '@prisma/client';
+import { deleteFile } from './services/aws';
+
+const prisma = new PrismaClient()
+
+const deleteUnsafeFilesCron = cron.schedule("* * * * *", async () => {
+    const files = await prisma.file.findMany({
+        where: {
+            safe: false
+        }
+    })
+    await prisma.file.deleteMany({
+        where: {
+            safe: false
+        }
+    })
+    for (const file of files) {
+        await deleteFile(file.key)
+    }
+})
+
+export default deleteUnsafeFilesCron;
