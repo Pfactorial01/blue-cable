da6aab04f48599ce7fd5a5a6c0c665d036fea410
Adebayo Adeoye <adeoyeadebayo18@gmail.com>
2023-08-25 19:27:41 +0100

    Converted to AWS sdk v3

--- src/services/aws/index.ts
+++ src/services/aws/index.ts
@@ -1,119 +1,118 @@
-import aws from "aws-sdk";
+import { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand, HeadObjectCommand } from "@aws-sdk/client-s3";
+import {S3ReadStream} from 's3-readstream';
 import { Request, Response } from "express";
 import multer from "multer";
 import dotenv from 'dotenv';
-
+import {Readable} from 'stream';
 
 dotenv.config();
 
 const awsBucket = process.env.AWS_BUCKET_NAME as string
 const awsSecret = process.env.AWS_SECRET_KEY as string
 const awsAccessKey = process.env.AWS_ACCESS_KEY as string
+const awsRegion = process.env.AWS_BUCKET_REGION as string
 
-const s3 = new aws.S3({
-  secretAccessKey: awsSecret,
-  accessKeyId: awsAccessKey
+const s3 = new S3Client({
+    region: awsRegion,
+    credentials: {
+        accessKeyId: awsAccessKey,
+        secretAccessKey: awsSecret
+    }
 });
 
 export const upload = multer({
   limits: {
     parts: Infinity,
-    fileSize: 1024 * 1024 * 1 
+    fileSize: 1024 * 1024 * 200
   },
   storage: multer.memoryStorage()
 });
 
 export const fileUpload = async (file: Express.Multer.File, Key: string) => {
-  const options = {
-    Bucket: awsBucket,
-    Key,
-    Body: file.buffer,
-  }
-  return await s3.putObject(options).promise();
+    const options = {
+        Bucket: awsBucket,
+        Key,
+        Body: file.buffer,
+    }
+    const command = new PutObjectCommand(options)
+    return await s3.send(command)
 }
 
 export const createFolderFn = async (Key: string) => {
-  const options = {
-    Bucket: awsBucket,
-    Key: `${Key}/`
-  }
-  return await s3.putObject(options).promise();
-}
-
-export const checkIfFolderExists = async (Key: string): Promise<boolean> => {
-  try {
     const options = {
       Bucket: awsBucket,
       Key: `${Key}/`
     }
-    await s3.headObject(options).promise();
-    return true;
-  } catch (error) {
-    return false;
+    const command = new PutObjectCommand(options)
+    return await s3.send(command)
   }
+
+export const checkIfFolderExists = async (Key: string): Promise<boolean> => {
+    try {
+      const options = {
+        Bucket: awsBucket,
+        Key: `${Key}/`
+      }
+      const command = new HeadObjectCommand(options)  
+      await s3.send(command)    
+      return true;
+    } catch (error) {
+      return false;
+    }
 }
 
 export const checkIfFileWithNameExists = async (Key: string): Promise<boolean> => {
-  try {
-    const options = {
-      Bucket: awsBucket,
-      Key: `${Key}`
+    try {
+      const options = {
+        Bucket: awsBucket,
+        Key
+      }
+      const command = new HeadObjectCommand(options)  
+      await s3.send(command)   
+      return true;
+    } catch (error) {
+      return false;
     }
-    await s3.headObject(options).promise();
-    return true;
-  } catch (error) {
-    return false;
-  }
 }
 
-// export const deleteSingleFile = async (
-//   req: Request,
-//   filepath: string
-// ): Promise<void> => {
-//   const params = { Bucket: awsBucket, Key: filepath };
-
-//   await s3.deleteObject(params, (err) => {
-//     if (err) throw new Error(err.message);
-//     else logger.info(formatLog(req, `Deleted file with key "${filepath}"`));
-//   });
-// };
-
 export const fileDownload = async (
-  req: Request,
-  res: Response,
-  filepath: string
-): Promise<void> => {
-  const options = {
-    Bucket: awsBucket,
-    Key: filepath
-  };
-
-  res.attachment(filepath);
-  const fileStream = s3.getObject(options).createReadStream();
-  fileStream.pipe(res);
+    req: Request,
+    res: Response,
+    filepath: string
+  ): Promise<void> => {
+    const options = {
+      Bucket: awsBucket,
+      Key: filepath,
+    };  
+    res.attachment(filepath);
+    const command = new GetObjectCommand(options)
+    const file = await s3.send(command);
+    const fileStream = file.Body as Readable
+    fileStream.pipe(res);
 };
 
-// export const deleteMultipleFiles = async (
-//   req: Request,
-//   filepaths: { Key: string }[]
-// ): Promise<void> => {
-//   logger.info(
-//     formatLog(
-//       req,
-//       `Deleting files with filepaths "${JSON.stringify(filepaths)}"`
-//     )
-//   );
+export async function createAWSStream(req: Request, res: Response, Key: string) {
+    const bucketParams = {
+        Bucket: awsBucket,
+        Key
+    }
+    const headObjectCommand = new HeadObjectCommand(bucketParams);
+    const headObject = await s3.send(headObjectCommand);
+    const maxLength = headObject.ContentLength as number;
+    const options = {
+    s3,
+    command: new GetObjectCommand(bucketParams),
+    maxLength,
+    byteRange: 1024 * 1024
+    };
+    const stream = new S3ReadStream(options);
+    return stream
+}
 
-//   const params = { Bucket: awsBucket, Delete: { Objects: filepaths } };
-
-//   await s3.deleteObjects(params, (err) => {
-//     if (err) throw new Error(err.message);
-//     else
-//       logger.info(
-//         formatLog(
-//           req,
-//           `Deleted files with filepaths "${JSON.stringify(filepaths)}`
-//         )
-//       );
-//   });
-// };
+export const deleteFile = async (
+  filepath: string
+): Promise<void> => {
+  const params = { Bucket: awsBucket, Key: filepath };
+  const command = new DeleteObjectCommand(params);
+  await s3.send(command);
+};
